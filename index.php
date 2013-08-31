83a06d1ff4382f846b15866219b1360c0b514cc5
Dominik Janata <dominik.janata@gmail.com>
2013-08-31 18:35:16 +0200

    Hotová autentikace, funkční stahování informací

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
+    //header("Content-type: text/plain; charset=utf-8");
     include "mirror-quickstart-php-master/index.php";
+    // vikitron.com/api.php?q=12
+    //$content = file_get_contents('http://www.vikitron.com/api.php?q=12');
+    
+    //echo $content;
 ?>
